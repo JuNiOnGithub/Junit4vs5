846aeaa61fe0118926eae42d2b775ef4b3fbc8ff
JuNi <JuNi@testing.com>
2018-12-18 11:30:24 +0100

    new test annotation

--- src/test/java/CarTests.java
+++ src/test/java/CarTests.java
@@ -1,30 +1,37 @@
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.HashMap;
 
 
 public class CarTests {
-    private HashMap<Integer, Car> repository = new HashMap<>();
+    private static HashMap<Integer, Car> repository = new HashMap<>();
 
 
 
 
-@Before
-    public void initTest() {
+@BeforeClass
+// Doit être static si on souhaite @BeforeClass
+    public static void initTest() {
     System.out.println("Hello, this is the test init");
     Car car = new Car();
     car.setName("Test");
     Car car2 = new Car();
-    car.setName("Test2");
+    car2.setName("Test2");
     repository.put(1,car);
     repository.put(2,car2);
 }
 
 @After
     public void displayAll() {
-    System.out.println(repository.toString());
+    System.out.println("Displaying informations :");
+    repository.forEach((k,v) -> System.out.println("key : "+k+" value : "+v));
+}
+
+@Test
+@Ignore
+    public void isVoitureVolante() {
+    System.out.println("On ne passe pas ici.  :) ");
+    // Assert.assertTrue(repository.get(1).isVoitureVolante());
 }
 
 @Test
